d710a6c780c8c2ec4767d98bd75b030d57486bd1
Hussain Tech Labz <102964719+MAbdullahNA@users.noreply.github.com>
2025-07-21 19:06:59 +0500

    save/show userName and userAge in PlayerPref

---
+++ Assets/Demos/Physics/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public InputField userName, userAge;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        userName.text = PlayerPrefs.GetString("userName");
+        userAge.text = PlayerPrefs.GetString("userAge");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SaveUserDataBtnClick()
+    {
+        PlayerPrefs.SetString("userName", userName.text);
+        PlayerPrefs.SetString("userAge", userAge.text);
+    }
+}
